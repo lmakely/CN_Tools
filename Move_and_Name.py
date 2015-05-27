fe62bebb02f969f29e2e900c2b726f387863ad6f
Lauren Makely <lmakely09@gmail.com>
2015-05-27 09:51:28 -0400

    Updated Move_and_Name
    also moved old reference scripts to different folder

--- Move_and_Name.py
+++ Move_and_Name.py
@@ -1,42 +1,73 @@
+# Author: Lauren Makely (CN) and Jeff Ely (NASA DEVELOP)
+# Created: May 22, 2015
+# Last Update: May 27, 2015
+
 import os, shutil
 from dnppy import core 
 
-works = r"C:\LMM\ArcTools\test folder\uptp"
+works = r"C:\LMM\ArcTools\test_folder"
 
 def sort(filelist):
+    # moves files to a folder named after them
+    # must decide which part of the filename you want
+    # Script will ask you for a project number in order to format the
+    #   the name of the folder correctly
+
+    # determines the project naming convention
+    project = input("Which project number are you working on?")
+
+    # finds all the files in the workspace folder provided
+    # if you are only looking for a certain type of file extension,
+    # replace the 2nd False with strings (ex: ".shp") or a list of
+    # strings (ex: [".shp", ".xml"]
+    # The same can be done with the last False to exclude file types
     files = core.list_files(False, filelist, False, False)
+    
     for filename in files:
        head,tail = os.path.split(filename)
+    
+       if project == 5646:
+           tail_list = tail[0:4]
+           print "Beginning to move " + tail
+           move_dir = os.path.join(head, tail_list)
 
-       tail_list = tail[6:10]
+           if not os.path.exists(move_dir):
+               os.makedirs(move_dir)
 
-       print "Beginning to move " + tail
-       move_dir = os.path.join(head, tail_list)
+           print tail + " moved to folder"
+           shutil.move(filename, os.path.join(move_dir, tail))
 
-       if not os.path.exists(move_dir):
-           os.makedirs(move_dir)
+       elif project ==  5088:
+           delim = "_"
+           tail_list = tail.split(delim)
+                              
+           print "Moving " + tail
+           move_dir = os.path.join(head, tail_list[0])
 
-       print tail + " moved to folder"
-       shutil.move(filename, os.path.join(move_dir, tail))
+           if not os.path.exists(move_dir):
+               os.makedirs(move_dir)
 
+           print tail + " moved to folder"
+           shutil.move(filename, os.path.join(move_dir, "_".join(tail_list[1:])))
+           print("Moved file '{0}' ".format(filename))
+           
+    print("Moved all files!")
     return
 
-##print "Sorting files"
-##sort(works)
+sort(works)
 
-def NewFileName (recursive, workspace, installation, util):
+def NewFileName (recursive, workspace, installation):
     files = core.list_files(recursive, workspace)
-    print files
+    util = os.path.dirname(workspace)
     i = 100
-    newname = installation + "-" + util + "-000"
+
+    newname = "{0}-{1}-{2}".format(installation, util,i.zfill(6))
     
     for items in files:
         print items
         original = items[:-4]
-        new= newname + str(i)
-        print "Renaming " + original + " to " + new
-        core.rename(items, original, workspace + "\\" + new)
+        print "Renaming " + original + " to " + newname
+        core.rename(items, original, workspace + "\\" + newname)
         i = i +1
 
-
-NewFileName(False,works, "13C40", "uptp")
+# ex: NewFileName(True, works, "13A80")
--- rename.py
+++ rename.py
@@ -3,35 +3,29 @@ import os
 
 
 # Help:
-#     !!WARNING!!   Currently this script will rename ALL files in that folder
-#                   I'm working on making it recognized already correct names
+#   !!WARNING!!   Currently this script will rename ALL files in a folder
+#                 It will use the first 4 characters to determine the util
+#                 Make a backup of files just in case!
+#
 #   workspace       file path to folder containing items to be renamed
 #   installation    Installation code (string)
-#   util            The utility code (string)
+#   util            The utility code (string) determined by current name
 
 def NewFileName (recursive, workspace, installation):
-    if os.path.isdir(workspace):
-        print "Directory exists, will begin processing files"
-    else:
-        print "Folder not found. Please check for typos and make sure you have access to the proper drives."
-
     files = core.list_files(recursive, workspace)
     i = 100
+    head, tail = os.path.split(files[0])
+    util = tail[0:4]
+    newname = installation + "-" + util + "-000"
     
     for items in files:
-        findFolder = os.path.dirname(items)
-        print "Found: " + items
+        print items
         original = items[:-4]
-        head, util = os.path.split(findFolder)
-
-        newname = installation + "-" + util + "-000"
         new= newname + str(i)
         print "Renaming " + original + " to " + new
         core.rename(items, original, workspace + "\\" + new)
         i = i +1
 
-            
+works = r"C:\LMM\ArcTools\test_folder"
 
-
-works = r"C:\LMM\ArcTools\test folder"
-NewFileName(True, works, "13C40")
+NewFileName(False, works, "13A80")
