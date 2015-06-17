8ba8f44264b7b3a13f552ac128da1f15bf3be494
Lauren Makely <lmakely09@gmail.com>
2015-06-17 15:42:02 -0400

    Fixed syntax error in field calculator

--- Match_XLS_to_Attributes.py
+++ Match_XLS_to_Attributes.py
@@ -9,70 +9,47 @@ import shutil
 import arcpy
 from xls_class import xls_class as xc
 from pprint import pprint
+import read_att_table
 
 wrkbk1 = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\Matching_XL.xlsx"
 wrkbk2 = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\fields_on.xlsx"
 shp = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\shps\CAD_Polygons_Gaeta_SE.shp"
-
-def backup_shapefile(source_shp):
-    """ copies a shapefile and all its metadata """
-
-    metalist = [".cpg", ".dbf", ".prj", ".shp.xml" ,".shx"]
-    head, tail = os.path.split(source_shp)
-
-    copylist = [source_shp]
-    for metaitem in metalist:
-        copylist.append(source_shp.replace(".shp",metaitem))
-
-    # create backupdir
-    backupdir = os.path.join(head,"{0}_backup".format(tail.replace(".shp","")))
-    if os.path.isdir(backupdir):
-        shutil.rmtree(backupdir)
-        os.makedirs(backupdir)
-    else:
-        os.makedirs(backupdir)
-
-    # copy files into the directory
-    for copy in copylist:
-        shutil.copy(copy, os.path.join(backupdir, os.path.basename(copy)))
-
-    print("Backup created at {0}".format(backupdir))
-    return
 
 def read_attributes(shp_filepath, in_list):
     """
     Returns a python table of input shapefiles attribute table
     """
 
-    # create a backup of the shapefile
-    #backup_shapefile(shp_filepath)
-    
     # find the field names
     field_names = []
     faillist = []
     fields      = arcpy.ListFields(shp_filepath)
 
+    # ID fields in shpaefile
     print("Shapefile has the following fields:")
     for field in fields:
         print field.baseName
         field_names.append(field.baseName)
         
     print "--------------------------------"
-    
+
+    # defines xlsx
     onsheet = xc()
     onsheet.read(in_list)
 
+    # read xlsx to print headers to be joined
     print "Searching for desired fields: "
     wanted = onsheet.worksheets["CAD_SDS"][0,2:8]
     print " " + ", ".join(wanted)
 
     print "--------------------------------"
 
-    expression = "[" + "]+[".join(wanted) + "]"
+    expression = "[" + "]&[".join(wanted) + "]"
     print "Joining together " + expression
+    # make new field for query
     if "MatchField" not in field_names:
         arcpy.AddField_management(shp_filepath, "MatchField", field_type = "text")
-
+    # creates query statement
     arcpy.CalculateField_management (shp_filepath, "MatchField", expression)
     
 ##    with arcpy.da.SearchCursor (shp_filepath, "MatchField") as cursor:
@@ -80,7 +57,10 @@ def read_attributes(shp_filepath, in_list):
 ##            print row
     return
 
+
+#read_att_table.backup_shapefile(shp)
 backup_shp =r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\NDM_302_Schema_UTM33_EGM08_GaetaSE_working\NDM_302_Schema_UTM33_EGM08_GaetaSE_working.gdb\CAD_Temporary\CAD_Polygons_Gaeta_SE"
+
 read_attributes(backup_shp, wrkbk1)
 
 ### Pseudo Code below:
