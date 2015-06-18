3ab3745fddc07b12a02928b128d569995dd0f82d
Lauren Makely <lmakely09@gmail.com>
2015-06-18 17:12:38 -0400

    added more plans

--- Match_XLS_to_Attributes.py
+++ Match_XLS_to_Attributes.py
@@ -8,7 +8,6 @@ import os
 import shutil
 import arcpy
 from xls_class import xls_class as xc
-from pprint import pprint
 import read_att_table
 
 wrkbk1 = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\Matching_XL.xlsx"
@@ -17,7 +16,7 @@ shp = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Work
 ##mxd = arcpy.mapping.MapDocument("V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\5637_testspace.mxd")
 
 
-def Match_attributes(shp_filepath, in_list):
+def Match_attributes(geodb, shp_filepath, in_list):
     """
     Returns a python table of input shapefiles attribute table
     """
@@ -43,20 +42,31 @@ def Match_attributes(shp_filepath, in_list):
     onsheet = xc()
     onsheet.read(in_list)
 
+    # would it be easier to import this as a table at this point and read that?
+    TableToTable_conversion (in_list, geodb, "MatchingXL")
     # read xlsx to print headers to be joined
     print "Searching for desired fields: "
-    wanted = onsheet.worksheets["CAD_SDS"][0,2:8]
-    print " " + ", ".join(wanted)
 
+    # with xlrd
+    #wanted = onsheet.worksheets["CAD_SDS"][0,2:8]
+    #print " " + ", ".join(wanted)
+
+    # with arcpy
+    table = gdb + "\MatchingXL"
+    wanted = arcpy.ListFields(table)
+    wanted_names = []
+    for field in wanted:
+        wanted_names.append(field.baseName)
+        
     print "--------------------------------"
 
-    expression = "[" + "]&[".join(wanted) + "]"
-    print "Joining together " + expression
-    # make new field for query
-
+    # make new fields for querying
     if "LC_Code" not in field_names:
         arcpy.AddField_management(shp_filepath, "LC_Code", field_type = "short")
+        print "LC_Code attribute created"
+        print "Calculating LC_Code"
         # add code here to convert microstation types to values once field is created
+        # and a progress bar of some kind
     ##      Codeblock:
     ##        if Linetype = "Continuous":
     ##            return 0
@@ -75,14 +85,20 @@ def Match_attributes(shp_filepath, in_list):
     ##    arcpy.CalculateField_management (shp_filepath, "LC_Code", codeblock)
 
     if "MatchField" not in field_names:
-        arcpy.AddField_management(shp_filepath, "MatchField", field_type = "text")        
+        arcpy.AddField_management(shp_filepath, "MatchField", field_type = "text")
+        print "MatchField attribute created"
     elif:      # creates feature query statement as a field
+        expression = "[" + "]&[".join(wanted_names) + "]"
+        print "Joining together " + expression
         arcpy.CalculateField_management (shp_filepath, "MatchField", expression)
+        print "MatchField attribute calculated"
 
-    
+    print "--------------------------------"
 
     # join Matching_XL
     #arcpy.JoinField_management (shp_filepath, "MatchField", in_list, "MatchingField", ["Feature_Class"])
+
+    # select by attribute the feature_classes that are the same
 
 ##    with arcpy.da.SearchCursor (shp_filepath, "MatchField") as cursor:
 ##        for row in cursor:
@@ -94,14 +110,9 @@ def Match_attributes(shp_filepath, in_list):
 
 #read_att_table.backup_shapefile(shp)
 backup_shp =r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\NDM_302_Schema_UTM33_EGM08_GaetaSE_working\NDM_302_Schema_UTM33_EGM08_GaetaSE_working.gdb\CAD_Temporary\CAD_Polygons_Gaeta_SE"
+gdb = 
 
 Match_attributes(backup_shp, wrkbk1)
-
-### Pseudo Code below:
-##  if Level_Name == " "
-##        faillist.append(Level_Name)
-##  if len(CO) > 1 or len(CO)=0:
-##        faillist.append(Level_Name)
 
 
 
