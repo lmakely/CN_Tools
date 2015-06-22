dd45fb4f6a99a6f562addcd8184d2896c9f689bc
Lauren Makely <lmakely09@gmail.com>
2015-06-22 18:37:17 -0400

    check for errors

--- RunXLSXQuery.py
+++ RunXLSXQuery.py
@@ -1,30 +1,37 @@
 __author__ = 'lmakely'
 
-from xls_class import xls_class as xc
+from xls_class import xls_class
+import arcpy
+arcpy.env.overwriteOutput = True
 
 def SelectFeatures(input_xls, input_gdb):
 
-  # defines excel document and creates empty list for queries
-    input_xls = xc()
+    # defines excel document and creates empty list for queries
+    xc = xls_class()
     xc.read(input_xls)
-    query_list = []
 
-  # creates a list of queries to perform on the shapefile
-    query_list.append(xc.worksheets["CAD_SDS"][1:,13])
+    # creates a list of queries to perform on the shapefile
+    query_list = xc.worksheets["CAD_SDS"][1:,13]
 
-  for query in query_list:
-      
-    return
 
-def ExportSelection():
+    for layer in mapLayers:
+        desc = arcpy.Describe(layer)
+        print "Querying " + desc.name
+
+        i = 1
+        for query in query_list:
+
+            new_name = xc.worksheets["CAD_SDS"][i, 12]
+            print "Exporting to feature class: " + new_name
+            arcpy.Select_analysis(layer, new_name, query)
+            i = i + 1
 
     return
 
 if __name__ == "__main__":
 
-    arcpy.env.overwriteOutput = True
-    working_gdb = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\NDM_302_Schema_UTM33_EGM08_GaetaSE_working\NDM_302_Schema_UTM33_EGM08_GaetaSE_working.gdb")
-    mxd = arcpy.mapping.MapDocument(r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\5637_testspace"
+    working_gdb = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\NDM_302_Schema_UTM33_EGM08_GaetaSE_working\NDM_302_Schema_UTM33_EGM08_GaetaSE_working.gdb"
+    mxd = arcpy.mapping.MapDocument(r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\5637_testspace.mxd")
     mapLayers = arcpy.mapping.ListLayers(mxd)
     xls_file = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\Cad_Label_to_SDS.xlsx"
 
