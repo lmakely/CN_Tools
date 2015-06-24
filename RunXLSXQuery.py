1162a0d720cf42b163695c5c830707f1860ceb92
Lauren Makely <lmakely09@gmail.com>
2015-06-24 11:17:42 -0400

    added sorting loops for different gdbs

--- RunXLSXQuery.py
+++ RunXLSXQuery.py
@@ -8,7 +8,7 @@ arcpy.env.overwriteOutput = True
 def CreateFieldFromXLSX(feature_class, new_attribute_name):
 
     field_names = []
-    fields      = arcpy.ListFields(shp_filepath)
+    fields      = arcpy.ListFields(feature_class)
 
     # get list of current fields
     for field in fields:
@@ -24,7 +24,7 @@ def CreateFieldFromXLSX(feature_class, new_attribute_name):
 
     return
 
-def SelectFeatures(input_xls, input_gdb, input_gdb_workdir, input_mxd, outdir):
+def SelectFeatures(input_xls, input_302gdb, input_gdb_302workdir, input_26gdb, input_mxd):
 
     mxd = arcpy.mapping.MapDocument(input_mxd)
     mapLayers = arcpy.mapping.ListLayers(mxd)
@@ -36,7 +36,7 @@ def SelectFeatures(input_xls, input_gdb, input_gdb_workdir, input_mxd, outdir):
     # creates a list of queries to perform on the shapefile
 
     print("Running query list on all layers")
-    for layer in mapLayers: #get rid of this loop
+    for layer in mapLayers:
         desc = arcpy.Describe(layer)
         print("Querying {0}".format(desc.name))
         b = 1
@@ -60,69 +60,105 @@ def SelectFeatures(input_xls, input_gdb, input_gdb_workdir, input_mxd, outdir):
                 if lw != "":
                     lw = int(lw)
 
-
-                if ref == "":
-                    query = """"LAYER" = '{0}' AND "LEVEL" = {1} AND "COLOR" = {2} AND "LINETYPE" = '{3}' AND "LINEWT" = {4}""".format(ln, lv, co, lt, lw)
+                if ref == "" or geo  == "line" or geo == "line 3d":
+                    query = """"LAYER" = '{0}' AND "LEVEL" = {1} AND "COLOR" = {2} AND "LINETYPE" = '{3}' AND "LYRLINEWT" = {4}""".format(ln, lv, co, lt, lw)
                 else:
-                    query = """"LAYER" = '{0}' AND "LEVEL" = {1} AND "COLOR" = {2} AND "LINETYPE" = '{3}' AND "LINEWT" = {4} AND "REFNAME" = '{5}'""".format(ln, lv, co, lt, lw, ref)
+                    query = """"LAYER" = '{0}' AND "LEVEL" = {1} AND "COLOR" = {2} AND "LINETYPE" = '{3}' AND "LYRLINEWT" = {4} AND "REFNAME" = '{5}'""".format(ln, lv, co, lt, lw, ref)
 
-                out_name = xc.worksheets["CAD_SDS"][b, 12].replace("_","") # make sure we do not NEED these underscores
+                out_SDS = xc.worksheets["CAD_SDS"][b, 12]
+                out_FC = xc.worksheets["CAD_SDS"][b, 13]
+                out_name = out_SDS + "\\" + out_FC
 
-                print("Searching feature class: " + out_name)
-                print os.path.join(outdir, out_name + ".shp")
+                if out_SDS == "":
+                    out_SDS = xc.worksheets["CAD_SDS"][b, 14]
+                    out_FC = xc.worksheets["CAD_SDS"][b, 15]
+                    out_name = out_SDS + "\\" + out_FC
+
+                print out_FC
                 print query
 
                 # for shapefiles that do not already exists
-                if not os.path.exists(os.path.join(outdir, out_name)):
+                if xc.worksheets["CAD_SDS"][b, 13] != "" :
+                    outdir = input_302gdb
+                    if not os.path.exists(os.path.join(outdir, out_name)):
 
-                    # pull the spatial reference information
-                    spat_ref = arcpy.Describe(input_gdb + "\\" + input_gdb_workdir).spatialReference
+                        # pull the spatial reference information
+                        spat_ref = arcpy.Describe(outdir + "\\" + out_FC).spatialReference
 
-                    # create an empty new featureclass
-                    arcpy.CreateFeatureclass_management(outdir, out_name, "POLYGON", spatial_reference = spat_ref)
+                        # create an empty new featureclass
+                        arcpy.CreateFeatureclass_management(outdir, out_name, "POLYGON", spatial_reference = spat_ref)
 
-                    # apply the query to select specific attributes
-                    arcpy.SelectLayerByAttribute_management(layer, "NEW_SELECTION", query)
+                        # apply the query to select specific attributes
+                        arcpy.SelectLayerByAttribute_management(layer, "NEW_SELECTION", query)
 
-                    # make new layer
-                    arcpy.FeatureClassToFeatureClass_conversion(layer, outdir, out_name)
+                         # make new layer
+                        arcpy.FeatureClassToFeatureClass_conversion(layer, outdir, out_name)
 
-                    # calculate description attribute
-                    CreateFieldFromXLSX(out_name, "Description"):
+                        # calculate description attribute
+                        CreateFieldFromXLSX(out_name, "Description")
 
-                # for shapefiles that do exist
+                    # for shapefiles that do exist
+                    else:
+                        # apply the query to select specific attributes
+                        arcpy.SelectLayerByAttribute_management(layer, "NEW_SELECTION", query)
+
+                        # add to existing layer
+                        arcpy.Append_management(layer, os.path.join(outdir, out_name))
                 else:
+                    outdir = input_26gdb
+                    if not os.path.exists(os.path.join(outdir, out_name)):
 
-                    # apply the query to select specific attributes
-                    arcpy.SelectLayerByAttribute_management(layer, "NEW_SELECTION", query)
+                        # pull the spatial reference information
+                        spat_ref = arcpy.Describe(outdir + "\\" + out_FC).spatialReference
 
-                    # add to existing layer
-                    arcpy.Append_management(layer, os.path.join(outdir, out_name))
+                        # create an empty new featureclass
+                        arcpy.CreateFeatureclass_management(outdir, out_name, "POLYGON", spatial_reference = spat_ref)
+
+                        # apply the query to select specific attributes
+                        arcpy.SelectLayerByAttribute_management(layer, "NEW_SELECTION", query)
+
+                         # make new layer
+                        arcpy.FeatureClassToFeatureClass_conversion(layer, outdir, out_name)
+
+                        # calculate description attribute
+                        CreateFieldFromXLSX(out_name, "Narrative")
+
+                    # for shapefiles that do exist
+                    else:
+                        # apply the query to select specific attributes
+                        arcpy.SelectLayerByAttribute_management(layer, "NEW_SELECTION", query)
+
+                        # add to existing layer
+                        arcpy.Append_management(layer, os.path.join(outdir, out_name))
+                        # make new layer
+                        arcpy.FeatureClassToFeatureClass_conversion(layer, outdir, out_name)
+
+                        # calculate description attribute
+                        CreateFieldFromXLSX(out_name, "Narrative")
 
                 print out_name + " exported"
 
             except:
                 print("==== this is clearly Will's fault ====")
-                
-            b += 1
+
+        b += 1
 
     return
-
-
 
 if __name__ == "__main__":
 
     # laurens test area
-    #working_gdb = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\NDM_302_Schema_UTM33_EGM08_GaetaSE_working\NDM_302_Schema_UTM33_EGM08_GaetaSE_working.gdb"
-    #mxd = arcpy.mapping.MapDocument(r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\5637_testspace.mxd")
-    #mapLayers = arcpy.mapping.ListLayers(mxd)
-    #xls_file = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\Cad_Label_to_SDS.xlsx"
-    #SelectFeatures(xls_file, working_gdb, mapLayers)
+    gdb302 = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\NDM_302_Schema_UTM33_EGM08_GaetaSE_working.gdb"
+    gdb26 = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\NDM_26_Schema_UTM33_EGM08_GaetaSE_working.gdb"
+    gdbdir = "CAD_Temporary"
+    mxd = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\5637_testspace.mxd"
+    xls = r"V:\Projects\5637-GIS JV - Aerial Mapping for NSA Naples\Development\Working\LMM\Modified_Cad_Label_to_SDS.xlsx"
+    SelectFeatures(xls, gdb302, gdbdir, gdb26, mxd)
 
     # jeffs test area
-    gdb    = r"C:\Users\Jeff\Desktop\Github\CN_Tools\Reference\NDM_302_Schema_UTM33_EGM08_GaetaSE_working\NDM_302_Schema_UTM33_EGM08_GaetaSE_working.gdb"
-    gdbdir = "CAD_Temporary"
-    mxd    = r"C:\Users\Jeff\Desktop\Github\CN_Tools\Reference\5637_testspace.mxd"
-    xls    = r"C:\Users\Jeff\Desktop\Github\CN_Tools\Reference\Cad_Label_to_SDS.xlsx"
-    outdir = gdb
-    SelectFeatures(xls, gdb, gdbdir, mxd, outdir)
+    #gdb    = r"C:\Users\Jeff\Desktop\Github\CN_Tools\Reference\NDM_302_Schema_UTM33_EGM08_GaetaSE_working\NDM_302_Schema_UTM33_EGM08_GaetaSE_working.gdb"
+    #gdbdir = "CAD_Temporary"
+    #mxd    = r"C:\Users\Jeff\Desktop\Github\CN_Tools\Reference\5637_testspace.mxd"
+    #xls    = r"C:\Users\Jeff\Desktop\Github\CN_Tools\Reference\Cad_Label_to_SDS.xlsx"
+    #outdir = gdb
+    #SelectFeatures(xls, gdb, gdbdir, mxd, outdir)
