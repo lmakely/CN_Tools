44a5b92f6db43fa0b9cfc23d464483bfb8a806ad
Lauren Makely <lmakely09@gmail.com>
2015-12-29 19:05:25 -0500

    general layout for AttributeTransfer

--- 5088_AttibuteTransfer.py
+++ 5088_AttibuteTransfer.py
@@ -8,22 +8,35 @@ import os
 
 # this function adds up numbers from points shp that intersect a polygon in a shp
 # inserts number into polygon shp
-def AddUpBBHBs():
-
+def SumAttibuteAndUpdatePolygon():
+    if PPV_Unit_Number
 
 # identifies the lowest identifying number in a set of points inside a shapefile
-# sets street number to that number and then calculates mailing address and sds feature description
-# working on how to identify PPV unit number that need to be identified. 
-def LowestAddress():
+# sets street number to that number and then calculates Mailing_Address and sdsfeaturedescription
+# working on how to identify PPV unit number that need to be identified.
+def IdentifyLowestNumber():
 
 
-def SumUnits():
+# Adds up the number of points that lie within a polygon and updates Facility_Unit_Quantity
+def SumPointsAndUpdatePolygon():
 
 
+
+# Runs all three scripts
 def AttributeTransfer():
-    AddUpBBHBs()
-    LowestAddress()
-    SumUnits()
+    print "     Beginning to calculate facility bedrooms, bathrooms, and half-baths"
+    SumAttibuteAndUpdatePolygon()
+    print "     Completed updating the bedrooms, bathrooms, and half-bath counts"
+    print "-------------------------------------------------------------------------"
+    print "     Beginning to identify the lowest address number"
+    IdentifyLowestNumber()
+    print "     Completed updating the lowest addresses"
+    print "-------------------------------------------------------------------------"
+    print "     Beginning to calculate the Facility_Unit_Quantity"
+    SumPointsAndUpdatePolygon()
+    print "     Completed updating the Facility_Unit_Quantity"
+    print "-------------------------------------------------------------------------"
+
 
 # testing area
 if __name__ == "__main__":
